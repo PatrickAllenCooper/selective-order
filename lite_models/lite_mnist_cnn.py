a5a230cdbfa5edd0bb86374da72ac6a44de92fc6
PatrickAllenCooper <patrick.allen.cooper@gmail.com>
2020-08-16 16:27:44 -0600

    Lite models added for mc testing.

---
+++ lite_models/lite_mnist_cnn.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""Intended to deploy be used to generate the required header for deployment onto test stm boards. Makes use
+of tensorflow lite convertor. Ingests saved adversarial results and does not generate them itself.
+This is a distinct process from the purely experimental studies. Verification across a range of tasks will
+be provided."""
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+NUM_CLASSES = 10
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
+
+
+def build_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(100, activation='relu', kernel_initializer='he_uniform'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(
+        loss='sparse_categorical_crossentropy',
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        metrics=['accuracy'],
+    )
+    return model
+
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+baseline_model = build_model()
+
+baseline_model.fit(
+    ds_train,
+    epochs=15,
+    validation_data=ds_test
+)
